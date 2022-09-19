85a27d66f3d746c816f6a89d40c60bde5c51136f
tejasvi <pateltejasvi329@gmail.com>
2022-09-19 12:42:34 +0530

    background complete

--- app/src/main/java/com/example/madpractical6_20012021017/youtube.kt
+++ app/src/main/java/com/example/madpractical6_20012021017/youtube.kt
@@ -2,10 +2,18 @@ package com.example.madpractical6_20012021017
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 
 class youtube : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_youtube)
+        val youtubeid = ""
+        val mywebview = findViewById<WebView>(R.id.webview)
+        val settings = mywebview.settings
+        settings.javaScriptEnabled = true
+        settings.loadWithOverviewMode = true
+        settings.useWideViewPort = true
+        mywebview.loadUrl("https://www.youtube.com/embed/$youtubeid")
     }
 }
